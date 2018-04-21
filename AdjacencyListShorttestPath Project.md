2e5aa08a75e19fbc03620c6d6f72c2bd9241bcf5
Avery Zamzow <zamzowavery@yahoo.com>
2018-04-21 16:22:12 -0500

    First commit

---
+++ AdjacencyListShorttestPath Project.md
@@ -0,0 +1,35 @@
+# AdjacencyList/Shortest Path Project
+
+### Description 
+
+This project finds a path from the entrance to the exit in a maze that consists of rooms connected by doors. Each room is enumerated by consecutive integers, from 0 to n^2 - $$1$$, and it can have no more than 4 doors oriented to North , South , East  or West . The entry to the maze starts at the room 0 and ends at the room n^2 - 1. The maze can be modeled by an undirected graph, where a vertex corresponds to a room and an edge corresponds to a door, see the maze below with the path and the corresponding graph for the maze.
+
+![Graph](/Users/averyzamzow/Desktop/AdjacencyList:ShorttestPath/Graph.png)
+
+### Output
+
+The program asks the user for the filename containing the input and the filename to place the output path on.  This program can read graph data from any user file. In my program there are two inputs, input1.txt and input2.txt. I also have two different output files, output1.txt and output2.txt. After the user enters the file names, displayed on the screen is the algorithm used finds a path from the entrance to the the exit of the maze while also saving it to the output file. Then the program allows the user to choose between 4 different options...
+
+1) Print the adjacency list
+
+2) Find the length of the shortest path
+
+3) Print all the verticies in the shortest path
+
+4) Quit
+
+If the user chooses 1, then the adjacency list is printed. If the user chooses 2, then the paths length is printed. If the user chooses 3, then the verticies (doors) are printed to make the shortest path. Finally if the user wishes to quit, they an press 4. 
+
+The input file used to show as an example looks like this…. using the number of rooms(nodes) as the first number, then the lists each node's 4 doors. If they have a 1, they are open, if they have a 0, they are closed. The doors are in order from North , South , East  or West. 
+
+So take line 2 for example, that is Room 0, and its only opening is the east door. SO using the input file from  input1.txt, its a 3x3 maze and shown in the output file is the shortest path.
+
+![OUTPUTinput1](/Users/averyzamzow/Desktop/AdjacencyList:ShorttestPath/OUTPUTinput1.png)
+
+The output on the terminal looks like TERMINAL1.png.
+
+![TERMINAL1](/Users/averyzamzow/Desktop/AdjacencyList:ShorttestPath/TERMINAL1.png)
+
+I am displaying the graph in the text format, use zero to denote a vertex (room) on the entry-exit path and x otherwise. As shown in output1.txt
+
+![OUTPUToutput1](/Users/averyzamzow/Desktop/AdjacencyList:ShorttestPath/OUTPUToutput1.png)
---
+++ Project.cpp
@@ -0,0 +1,211 @@
+/*
+Avery Zamzow
+221.202 
+alz005
+*/
+
+#include<iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include<fstream>
+
+using namespace std;
+
+int c = 0;
+
+void print_list(vector<vector<int> > graph , int n) 		//function prints adjacency list of graph 
+{															 
+    for(int i=0;i<graph.size();i++)							
+    {
+        cout << i << "-> " ;
+        for(int j=0;j<graph[i].size();j++)					//counts doors on nodes
+        {
+            if(graph[i][j] == 0)							// goes north
+            {
+                cout << i - n << " ";						//then node above (n is the square root of the node verticie)
+            }
+            else if(graph[i][j] == 1)						// goes east
+            {
+                cout << i + 1 << " ";						//then next node
+            }
+            else if(graph[i][j] == 2)						// goes south
+            {
+                cout << i + n << " ";						//then node below (n is the square root of the node verticie)
+            }
+            else											// goes west
+            {
+                cout << i - 1 << " ";						//then last node
+            }
+        }
+        cout<<endl;
+    }
+}
+
+vector<int> path(vector<vector<int> > graph ,int n)			// this function does a breadth first search on the graph
+{
+    queue<int > q;											//make a queue
+    vector<int> visited(n*n , 0);							// visited vector is created to mark the visited block
+    q.push(0);
+    vector<int> parent(n*n);								// parent block stores the parent of each block
+    parent[0] = -1;											// parent of first block is none so -1
+    while(!q.empty())
+    {
+        int v = q.front();
+        q.pop();
+        visited[v] = 1;	
+		c++;
+
+        for(int i=0;i<graph[v].size();i++)			// insert the child of the current verticy so all the doors which can be visited through this door
+        {													//checkes doors of each verticy
+            if(graph[v][i] == 0)							//if door on north
+            {
+                if(!visited[v - n])							//check to see if its visited
+                {
+                    q.push(v - n);							//push the node above (n is the square root of the node verticie)
+                    parent[v - n] = v;						//put it in the vector of the path
+                }
+            }
+            else if(graph[v][i] == 1)						//if door on east
+            {
+                if(!visited[v + 1])							//check to see if its visited
+                {
+                    q.push(v + 1);							//push the next node
+                    parent[v+1] = v;						//put it in the vector of the path
+                }
+            }
+            else if(graph[v][i] == 2)						//if door on south
+            {
+                if(!visited[v + n])							//check to see if its visited
+                {
+                    q.push(v + n);							//push the node below (n is the square root of the node verticie)
+                    parent[v+n] = v;						//put it in the vector of the path
+                }
+            }
+            else											//if door on west
+            {
+                if(!visited[v - 1])							//check to see if its visited
+                {
+                    q.push(v - 1);							//push the last node
+                    parent[v - 1] = v;						//put it in the vector of the path
+                }
+            }
+        }
+    } return parent;
+}
+
+vector<int> get_path(vector<int> parent, int n)				// this function return the vector of the path obtained from parent vector
+{
+    int i = n*n - 1;										//i is the exit node
+    vector<int> s;											//creates a new vector to put the shortest path
+    s.push_back(i);											
+    while(parent[i] != -1)									//while the path in the parent vector is not at the exit node
+    {
+        s.push_back(parent[i]);								//add a new element to the vector each time a new node is read
+        i = parent[i];										//incrament node
+    }
+    return s;
+}
+
+void display_path(vector<int> path ,int n)					// this function displays the path in output as well as create the file to store the output 
+{
+    cout << "Enter the filename for output : "<<endl;			//output file where you will put the path of the graph
+    string s;
+    cin >> s;
+    ofstream file(s);
+    cout << "-------------SHORTEST PATH--------------";			//prints to screen
+    file << "---------------SHORTEST PATH---------------";		//prints to file
+
+    sort(path.begin(),path.end());								//sorts the path
+    int k = 0;
+    for(int i = 0; i<n*n;i++)									//go through each verticy
+    {
+        if(i%n == 0)											//if end verticy then go to the next line
+        {	
+            cout << endl;										//prints to screen
+            file << endl;										//prints to file
+        }
+        if(i == path[k])										//if this node is on the path print 0
+        {
+            cout <<"O ";										//prints to screen
+            file <<"O ";										//prints to file
+            k++;
+        }
+        else													//if this node isnt on the path print X
+        {
+            cout << "X ";										//prints to screen
+            file << "X ";										//prints to file
+        }
+    }
+    file.close();
+    cout << endl;
+}
+
+int main() 										//main fucntion
+{
+    string s;									//creating a value s to hold the files name
+    cout << "Enter file name : "<<endl;
+    cin >> s;
+    fstream file(s);							//reads file input
+    vector<vector<int> > graph;					//represents the graph as an vector of vectors
+    int num;
+
+    file >> num;
+    int n = sqrt(num);
+    for(int i=0;i<num;i++)
+    {
+        vector<int> temp;						//initialize a graph according to the given number of nodes
+        graph.push_back(temp);					//initialize the graph
+        for(int j=0;j<4;j++)					//Read number of nodes and doors in the file
+        {										//j being doors
+            int curr;							
+            file >> curr;						//copy the file's node to curr
+            if(curr == 1)
+            {
+                graph[i].push_back(j);			// creates an adjacency list to stores all the doors
+            }
+        }
+    }
+
+    vector<int> parent = path(graph,n);			//creates a vector parent to search the graph for a path
+    vector<int> path = get_path(parent,n);		//Creates a vector path obtained from the parent vector
+    display_path(path,n);						//displays the path
+
+    while(1)									// simple menu function to print and list the graph
+    {
+        cout << "\nEnter the option : \n";
+        cout << "1 : Print adjacency list of graph\n2: Find length of the path\n3: Print all vertices in shortest path\n4: Quit\n";
+        int a;									// your option you want to choose
+        cin >> a;
+        switch(a)
+        {
+        case 1:									//Print adjacency list of graph
+			cout << "\n" << endl;
+            print_list(graph,n);
+            break;
+        case 2:									//Print all vertices in shortest path				
+        {
+            cout << "\nLength : " << path.size() << endl;	//prints the length of the path
+			cout << "How many times it traveres through : " << c << endl;
+            break;
+        }
+        case 3:									//Print all vertices in shortest path				
+        {
+			cout << "\n" << endl;
+            vector<int> s(path);				//creates a vector for the adjacency list and gets the path
+            for(int i=0;i<s.size();i++)			//print out the adjacency list based off size
+            {
+                cout << s[s.size()-i-1] << " ";
+            }
+            cout << endl;
+            break;
+        }
+        default:									//terminates the program
+            exit(1);
+
+        }
+    }
+
+    return 0;
+}
